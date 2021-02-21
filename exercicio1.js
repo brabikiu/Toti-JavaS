66d9d00bff4b19444e5195812b408dc223883d5e
Barbara Villegas <barbaraavillegas@gmail.com>
2021-02-21 20:29:03 -0300

    Adicionado exercicio Aula 3 JavaScript

---
+++ exercicio1.js
@@ -0,0 +1,31 @@
+
+let nome = prompt ("Escreva seu nome");
+let sobrenome = prompt ("Escreva seu sobrenome");
+let idade = prompt ("Escreva sua idade");
+let cerveja = prompt ("Gosta de cerveja? Escreva \"sim\" em caso de que goste ou \"não\" em caso de não gostar");
+
+
+let sim = true;
+let não = false;
+
+
+if(sim) 
+alert("O usuario gosta de cerveja");
+
+
+
+if(não) 
+alert("O usuario não gosta de cerveja");
+
+
+
+console.table(
+    { 
+Nome: "Gabriel",
+sobrenome:"Casares",
+idade: "39"
+
+
+}
+)
+
